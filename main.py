832f6d9e15d83498bb038dccc5bff48bff75e878
PrinceMishra2006 <mishrajii1476@gmail.com>
2025-04-28 15:16:13 +0530

    logger and exception

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from sensor.exception import SensorException
 import os 
 import sys 
 
+from sensor.logger import logging
+
 def test_exception():
     try:
+        logging.debug("ki yaha p bhaiaa ek error ayegi diveision by zero wali error")
         a=1/0
     except Exception as e:
         raise SensorException(e,sys)
